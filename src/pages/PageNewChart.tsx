19bffa7340179c5d9b399efaff995f9234fe3be2
Kate Novak <kje@cochleoid.com>
2023-10-15 16:40:27 -0400

    Forms
    Start of a forms data-gathering implementaiton.

--- src/pages/PageNewChart.tsx
+++ src/pages/PageNewChart.tsx
@@ -1,9 +1,20 @@
 import { PropsWithChildren } from "react";
 import { Header } from "../components/Header";
 import Page from "../components/Page";
+import Form from "../components/forms/Form";
 
 export default function PageNewChart(props: PropsWithChildren & { onClose: () => void }) {
   return <Page>
     <Header closable={true} onClose={props.onClose} />
+    <Form definitions={[
+      {
+        kind: 'input',
+        label: 'Name',
+      },
+      {
+        kind: 'input',
+        label: 'code',
+      },
+    ]} />
   </Page>;
 }
